91554633b86cd5d13d916a8e7f85471db5acb7dd
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-09-05 00:54:56 +0000

    Added algorithms

--- algorithms.js
+++ algorithms.js
@@ -1,5 +1,6 @@
-// CONVERT TO CELSIUS
-
+// ==============================
+// === CONVERT TO CELSIUS =======
+// ==============================
 function convertToF(celsius) {
   let fahrenheit;
 
@@ -9,7 +10,60 @@ function convertToF(celsius) {
   return fahrenheit;
 }
 
-convertToF(30);
+
+// ==============================
+// ===== REVERSE A STRING =======
+// ==============================
+function reverseString(str) {
+  let newStr = str.split("").reverse().join("");
+  
+  return newStr;
+}
 
 
+// ==============================
+// ======== FACTORIALIZE ========
+// ==============================
+function factorialize(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    num = num * factorialize(num-1);
+  }
+  return num;
+}
+
+// ==============================
+// ======== LONGEST WORD ========
+// ==============================
+function findLongestWordLength(str) {
+  let newStr = str.split(" ");
+  let longestWord = 0;
+
+  for ( let i = 0; i < newStr.length; i++) {
+    if (newStr[i].length > longestWord) {
+      longestWord = newStr[i].length;
+    }
+  }
+  return longestWord;
+}
+
+// ==============================
+// ====== LARGEST NUMBERS =======
+// ==============================
+function largestOfFour(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
+  }
+
+  return results;
+}
 
