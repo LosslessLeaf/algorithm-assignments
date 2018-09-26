24dee6b4cc0e497ec1d25c1b2ac309ced040099f
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-09-26 14:20:03 +0000

    Iterates through list of names and prints Hello "name"! to each

---
+++ loop.js
@@ -0,0 +1,6 @@
+let cohort5Names = ["Paul", "Dan", "Brett", "Josh", "Tom", "Neshan", "Alejandra", "Melissa", "Mick", "Christian", "Joe"];
+
+for (let i = 0; i < cohort5Names.length; i++) {
+    console.log("Hello " + cohort5Names[i] + "!");
+}
+
