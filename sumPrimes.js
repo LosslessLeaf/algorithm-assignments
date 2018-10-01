155b6d38e0fe2c885171ffaa2f933aaa9fc48d15
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-10-01 23:14:39 +0000

    Sums all prime numbers til given value

---
+++ sumPrimes.js
@@ -0,0 +1,25 @@
+function sumPrimes(num) {
+    let primeArrays = [];
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+        if (isPrime(i) === true) {
+            primeArrays.push(i);
+        }
+    }
+    for (let i = 0; i < primeArrays.length; i++) {
+        sum += primeArrays[i];
+    }
+
+    console.log(sum);
+}
+
+function isPrime(value) {
+    for (var i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
+
+sumPrimes(10);
