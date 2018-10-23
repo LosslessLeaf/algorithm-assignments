9932bff6dc3e6ca1c5d233e3be800f2862420b3a
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-10-23 22:57:13 +0000

    Refactored factorialize

--- factorialize.js
+++ factorialize.js
@@ -1,4 +1,22 @@
-const factorialize = num => num === 0 || num === 1 ? 1 : num = num * factorialize(num - 1);
+// const factorialize = num => num === 0 || num === 1 ? 1 : num = num * factorialize(num - 1);
 
+function factorialize(num) {
+    let factorial = num;
+    let updater = num;
+    if (num === 0 || num === 1) {
+        console.log(1);
+        return 1;
+    }
+    else {
+        for (let i = 1; i < num; i++) {
+            updater -= 1;
+            factorial *= updater;
+        }
 
-factorialize(5);
+        console.log(factorial);
+        return factorial;
+    }
+
+}
+
+factorialize(12);
