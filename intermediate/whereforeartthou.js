010f09076abda5fdc485487d0f1c9d3b63f39de1
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-12-13 02:49:15 +0000

    Added whereforeartthou.js

---
+++ intermediate/whereforeartthou.js
@@ -0,0 +1,27 @@
+function whatIsInAName(collection, source) {
+    var arr = [];
+    var checkerObj = Object.keys(source);
+
+    for (let i = 0; i < collection.length; i++) {
+        for (var sourceKey in source) {
+            var sourceKeyValue = source[sourceKey];
+        }
+        for (var n in collection[i]) {
+            var collectionKeyValue = collection[i][n];
+            if (collectionKeyValue === sourceKeyValue) {
+                var objToCheck = Object.keys(collection[i]);
+                    console.log(n);
+                    for (let j = 0; j < checkerObj.length; j++) {
+                        if (checkerObj[j] === n) {
+                            arr.push(collection[i]);
+                    }
+                }
+
+            }
+        }
+    }
+    console.log(arr);
+
+    return arr;
+}
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
